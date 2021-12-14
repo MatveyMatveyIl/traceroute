29fec5562d60f84c9a0a117f66fadc322c1a53c5
MatveyMatveyIl <matvey.ilichev@gmail.com>
2021-12-14 16:30:19 +0500

    package manager

--- traceroute.py
+++ traceroute.py
@@ -1,4 +1,8 @@
 import argparse
+from scapy.sendrecv import sr
+from scapy.layers.inet import IP, TCP, ICMP, UDP
+
+
 
 
 class Traceroute:
@@ -20,14 +24,36 @@ class Traceroute:
                             help='displaying the autonomous system number for each ip address')
         parser.add_argument('-p', type=int, action='store', help='port (for tcp or udp)')
         parser.add_argument('-n', type=int, action='store', default=20, help='maximum number of requests')
-        parser.add_argument('protocol', type=str, action='store', help='protocol{tcp|udp|icmp}')
+        parser.add_argument('protocol', type=str, action='store', help='protocol{tcp|udp|icmp}',
+                            choices=['tcp', 'udp', 'icmp'])
         return parser.parse_args()
 
     # endregion
 
     def find_route(self):
+        package_maker = self._protocol_manager()
         for i in range(1, self.num):
             pass
+
+
+    def _create_TCP_package(self, num):
+        pass
+
+    def _create_UDP_package(self, num):
+        pass
+
+    def _create_ICMP_package(self, num):
+        pass
+
+    def _protocol_manager(self):
+        if self.protocol == 'tcp':
+            return self._create_TCP_package
+        elif self.protocol == 'udp':
+            return self._create_UDP_package
+        elif self.protocol == 'icmp':
+            return self._create_ICMP_package
+        else:
+            raise ValueError('incorrect protocol')
 
 
 def main():
